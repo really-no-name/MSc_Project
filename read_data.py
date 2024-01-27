ba57d26887778680e76136180a9ab3fdbc5fc064
really-no-name <xu_bolun@126.com>
2024-01-27 18:10:02 +0000

    first code

---
+++ read_data.py
@@ -0,0 +1,11 @@
+from torch.utils.data import Dataset
+# from PIL import Image
+import os
+
+# class MyData(Dataset):
+#     def __init__(self, root_dir, label_dir):
+#         self.root_dir = root_dir
+#         self.label_dir = label_dir
+#         self.path = os.path.join(self.root_dir, self.label_dir)
+
+#     def __getitem__(self, index):
