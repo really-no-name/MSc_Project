3e705d0ca9429865bc6b73cfc241960106e78cf2
really-no-name <xu_bolun@126.com>
2024-01-29 09:54:29 +0000

    Data_Clean

--- Data_Clean.py
+++ Data_Clean.py
@@ -4,6 +4,13 @@ import numpy as np
 # 读取数据，使用空格作为分隔符
 df = pd.read_csv('all-mias/info/info.csv', sep='\\s+', header=None)
 
+# 在第一列的每个值前添加指定的字符串
+prefix = ".pgm"
+df[0] = df[0].astype(str) + prefix
+
+# 删除第一列中重复值所在的行，保留第一次出现的行
+df.drop_duplicates(subset=[0], keep='first', inplace=True)
+
 # 替换空缺的值为NaN
 df.replace('', np.nan, inplace=True)
 
--- read_data.py
+++ read_data.py
@@ -1,17 +1,22 @@
-from torch.utils.data import Dataset
-from PIL import Image
-import os
+import os  # 与操作系统进行交互
+import pandas as pd  # 处理 CSV 文件
+from PIL import Image  # (Python Imaging Library) 用于图像处理
+from torch.utils.data import Dataset  # 是 PyTorch 中用于创建自定义数据集的基类
 
 class MyData(Dataset):
-    def __init__(self, root_dir, label_dir):
+    def __init__(self, root_dir, csv_file):
         self.root_dir = root_dir
-        self.label_dir = label_dir
-        self.path = os.path.join(self.root_dir, self.label_dir)
-        self.img_path = os.listdir(self.path)
+        self.csv_file = pd.read_csv(csv_file)
+        self.img_names = self.csv_file.iloc[:, 0]  # 假设第一列是图片名称
 
     def __getitem__(self, index):
-        img_name = self.img_path[index]
-        img_item_path = os.path.join(self.root_dir,self.label_dir,img_name)
-        img = Image.open(img_item_path)
-        label = self.label_dir
-        return img, label
+        img_name = self.img_names[index]
+        img_path = os.path.join(self.root_dir, img_name)
+        img = Image.open(img_path)
+
+        # 读取 label_1 到 label_6
+        labels = self.csv_file.iloc[index, 1:7].values
+        return img, labels
+
+    def __len__(self):
+        return len(self.img_names)
