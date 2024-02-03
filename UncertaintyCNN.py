b39716b05d04b1336954d9a404442fe274b25541
really-no-name <xu_bolun@126.com>
2024-02-03 15:05:42 +0000

    network

--- UncertaintyCNN.py
+++ UncertaintyCNN.py
@@ -3,5 +3,9 @@ from torch import nn
 
 class UncertaintyCNN(nn.Module):
     def __init__(self):
+        super(UncertaintyCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
 
-    def forward(self):
+    def forward(self, x, apply_dropout=False):
+        return label1_pred, label2_pred
